233f3dc1f2a3a5e5f84ef9c0942f4d5a74370a3e
Aaron Fruth <atfruth@gmail.com>
2015-06-29 16:49:44 -0500

    database finished

---
+++ app/controllers/store.js
@@ -0,0 +1,14 @@
+var $ = require('jquery');
+var Backbone = require('backbone');
+Backbone.$ = $;
+
+var template = require("../views/store.hbs");
+
+module.exports = {
+  displayItems: function(){
+    return ;
+  },
+  displayItem: function(){
+    return ;
+  },
+}
